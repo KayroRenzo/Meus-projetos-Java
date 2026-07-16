73101dd39ec8facae0702b85cdf27026a2f12b29
Kayro Renzo <kayrorenzo08@gmail.com>
2026-07-16 18:55:13 -0300

    Adiciona Gerenciador de Tarefas com Prioridade

---
+++ GerenciadorDeTarefasComPrioridade/model/Categoria.java
@@ -0,0 +1,18 @@
+package POO.Projetos_POO.GerenciadorDeTarefasComPrioridade.model;
+
+public class Categoria {
+    private String categoria;
+
+    public Categoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+}
